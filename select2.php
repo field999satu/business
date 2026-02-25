61bc0e599d90b545b5b6d5c8ed5b3c462934aa21
field999satu <684259013@webmail.npru.ac.th>
2026-02-25 14:51:47 +0700

    3

--- select2.php
+++ select2.php
@@ -16,7 +16,7 @@
 
         while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo '<br>' .
-                'รหัสลูกค้าของฉันแบบที่ 1 : ' . $result['CustomerID'] . ' วันเกิด : ' . $result['Birthdate'] .
+                'รหัสลูกค้า : ' . $result['CustomerID'] . ' วันเกิด : ' . $result['Birthdate'] .
                 ' ยอดหนี้ : ' . $result['OutstandingDebt'];
         }
     } catch (PDOException $e) {
